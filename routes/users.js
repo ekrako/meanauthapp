b32525b71145e668c80d9ec0336249fdf9461055
Eran Krakovsky <ekrako@gmail.com>
2017-02-25 13:44:19 +0200

    Finished User Model & Register https://www.youtube.com/watch?v=1ZeDy2QI3OE&t=2s

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 //register
-router.get('/register',(req,res,next)=>{
-  res.send('REGISTER Route');
+router.post('/register',(req,res,next)=>{
+  let newUser = new User({
+    name:req.body.name,
+    email:req.body.email,
+    username:req.body.username,
+    password:req.body.password
+  });
+
+  User.addUser(newUser,(err, user)=>{
+    if(err){
+      res.json({success: false, msg:'Failed to register user' })
+    }else{
+      res.json({success: true, msg:'User registered' })
+    }
+
+  })
 });
 
 //authenticate
 router.get('/authenticate',(req,res,next)=>{
-  res.post('AUTHENTICATE Route');
+  res.send('AUTHENTICATE Route');
 });
 
 //Profile
@@ -16,8 +33,8 @@ router.get('/profile',(req,res,next)=>{
   res.send('PROFILE Route');
 });
 //Validate
-router.get('/validate',(req,res,next)=>{
-  res.send('VALIDATE Route');
-});
+// router.get('/validate',(req,res,next)=>{
+//   res.send('VALIDATE Route');
+// });
 
 module.exports = router;
